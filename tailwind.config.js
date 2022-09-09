129b31091ad2e260447ecf4fe598cedf9aee3d11
AKq <hnoofk14@gmail.com>
2022-09-09 21:01:30 +0300

    page of ice-cream

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'nunito': ['nunito', 'sans-serif'],
+      'MyFont': ['"My Font"', 'serif'] // Ensure fonts with spaces have " " surrounding it.
+    },
+  },
+  plugins: [],
+}
